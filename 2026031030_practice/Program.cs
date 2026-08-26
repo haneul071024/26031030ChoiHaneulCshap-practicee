ac5734fa5e2714437cb26e1da073a913b8b2e0d9
haneul071024 <a01039162341@gmail.com>
2026-08-26 12:42:42 +0900

    교수님 파일

---
+++ 2026031030_practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2026031030_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
