645887b67ef1e73d4cd5edd0934ad0525474bbea
haneul071024 <a01039162341@gmail.com>
2026-08-26 12:57:32 +0900

    이력서 연습

--- 2026031030_practice/Program.cs
+++ 2026031030_practice/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace _2026031030_practice
+﻿namespace _2026031030_practice // 그룹이름(카테고리) 범주
+
 {
-    internal class Program
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("나의 이력서");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("이름 : 최하늘");
+            Console.WriteLine("특징 : 개성이강함 진짜 강함");
         }
     }
 }
