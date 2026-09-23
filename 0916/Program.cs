46e592599895b042f8f6fd20c16b2b1ecfe738d8
새moonstone < a01039162341@gmail.com>
2026-09-23 12:14:02 +0900

    123

---
+++ 0916/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _0916
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //loop
+            //for
+            //while
+            //do-while
+            //foreach
+            //초기조건식 //유효성 //스텝
+            //for (int i = 0; i < 100; i++) //i,j,k,l,m,n
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            List <int> MyArray = new(){ 1, 2, 3, 4, 5 };
+
+            //for (int i = 0; i < MyArray.Count; i++)
+            //{
+            //    Console.WriteLine(MyArray[i]);
+            //}
+
+            //한글문자 출력
+            for (var= '가';
+        }
+    }
+}
